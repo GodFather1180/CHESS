f2b738ed9581cb5f9a758a086e6f09b6237ee176
GodFather1180 <tarundeep107@gmail.com>
2024-10-20 14:47:59 -0700

    sfml

--- main.cpp
+++ main.cpp
@@ -1,7 +1,8 @@
 #include <time.h>
+#include <SFML/Graphics.hpp>
 #include <iostream>
 
-int main (){
-    std::cout<< "Hello world" << std::endl;
+int main() {
+    std::cout << "Hello SFML!" << std::endl;
     return 0;
 }
