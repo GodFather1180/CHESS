455a3ac70e193e4e69f9386b53e181516860c17d
GodFather1180 <tarundeep107@gmail.com>
2024-10-26 21:27:33 -0700

    name space

--- main.cpp
+++ main.cpp
@@ -3,16 +3,16 @@
 #include <SFML/Graphics.hpp>
 #include <SFML/Window.hpp>
 #include <time.h>
+using namespace sf;
 
-
-void Draw_chessboard(sf::RenderWindow& window,int board,int size){
-    sf::RectangleShape();
-    sf::Color Whitecol(255,255,255);
-    sf::Color Blackcol(0,0,0);
+void Draw_chessboard(RenderWindow& window,int board,int size){
+    RectangleShape();
+    Color Whitecol(255,255,255);
+    Color Blackcol(0,0,0);
     
     for (int row =0; row<board;row++){
         for (int col=0; col<board;col++){
-            sf::RectangleShape square(sf::Vector2f(size,size));
+            RectangleShape square(Vector2f(size,size));
             square.setPosition(col*size,row*size);
             square.setFillColor(( row + col) %2 == 0 ? Whitecol : Blackcol);
             window.draw(square);
@@ -26,14 +26,14 @@ int main(){
     std::cout<< "Chess begins0!" << std::endl;
     int Cboard = 8;
     int Csize = 100;
-    sf::RenderWindow window(sf::VideoMode(Cboard*Csize,Cboard*Csize), "Chess Begins!");
+    RenderWindow window(VideoMode(Cboard*Csize,Cboard*Csize), "Chess Begins!");
 
     while (window.isOpen())
     {
-        sf::Event event;
+        Event event;
         while (window.pollEvent(event))
         {
-            if (event.type == sf::Event::Closed)
+            if (event.type == Event::Closed)
                 window.close();
         }
 
